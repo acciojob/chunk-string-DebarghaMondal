67d8c444473274d41064a0c7c1bc383909c46be5
DebarghaMondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-16 15:13:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function stringChop(str, size) {
   // your code here
+	 return str.match(new RegExp('.{1,' + size + '}', 'g'));
 }
-
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
